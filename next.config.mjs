495e5f52dacc96ad78549f2c0de65fb45d290fe0
heokwangyeop <heokwangyeop@gmail.com>
2026-02-11 15:42:44 +0900

    Fix Next build manifest copy error

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-/** @type {import('"'"'next'"'"').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 서버 런타임을 강제하여 static export를 막음
-  output: "standalone",
-  typescript: {
-    // 타입 에러가 있어도 프로덕션 빌드를 진행
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // ESLint 에러가 있어도 빌드 진행
-    ignoreDuringBuilds: true,
+  experimental: {
+    // Avoids missing client-reference-manifest when building on Vercel
+    serverMinification: false,
   },
 };
 
