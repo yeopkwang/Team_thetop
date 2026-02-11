1a42b063921c1c71ebce5d5cc1276f6a54a0bba0
heokwangyeop <heokwangyeop@gmail.com>
2026-02-11 15:06:26 +0900

    Mark photo/record pages as dynamic to skip prerender DB hits

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,4 +1,5 @@
-﻿import Link from "next/link";
+﻿export const dynamic = "force-dynamic";
+import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 
 export default async function PhotoPage() {
